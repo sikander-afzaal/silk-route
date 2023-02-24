3a4ae3e77a1f0b78a46860027d878eb9a17fe5ee
sikander-afzaal <sikander.aaa@gmail.com>
2023-02-24 15:25:21 +0500

    adasdasdasdasdas

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,10 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="wrapper bg-black py-5">
+    <footer className="wrapper bg-black pb-5 pt-8 lg:pt-16">
       <div className="contain flex-col justify-center items-center gap-5">
-        <div className="flex justify-between lg:flex-row flex-col items-center lg:gap-2  md:gap-5 gap-8 w-full">
-          <img src="/logo.png" className="w-[180px] object-contain" alt="" />
-          <div className="flex w-full justify-center items-center gap-4 md:gap-8 md:flex-row flex-col">
+        <div className="flex justify-between lg:flex-row flex-col relative items-center lg:gap-2  md:gap-5 gap-8 w-full">
+          <img
+            src="/logo.png"
+            className="w-[140px] lg:absolute left-0 top-1/2 lg:-translate-y-1/2 object-contain"
+            alt=""
+          />
+          <div className="flex justify-center w-full items-center gap-4 md:gap-8 md:flex-row flex-col">
             <a href="#" className="text-white text-base">
               Home
             </a>
